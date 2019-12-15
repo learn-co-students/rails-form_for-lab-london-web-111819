63d0d4ae6ab3fad38276da47b1a13b74be70d42a
Matthew <matthew@matthews-mbp.home>
2019-12-15 20:50:05 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Students RESTful routes
+  get '/students', to: 'students#index', as: 'students'
+
+  get '/students/new', to: 'students#new', as: 'new_student'
+
+  get '/students/:id', to: 'students#show', as: 'student'
+
+  post '/students', to: 'students#create', as: 'create_student'
+
+  get '/students/:id/edit', to: 'students#edit', as: 'edit_student'
+
+  patch '/students/:id', to: 'students#update', as: 'update_student'
+
+  # School Classes RESTful routes
+  get '/school_classes', to: 'school_classes#index', as: 'school_classes'
+
+  get '/school_classes/new', to: 'school_classes#new', as: 'new_school_class'
+
+  get '/school_classes/:id', to: 'school_classes#show', as: 'school_class'
+
+  post '/school_classes', to: 'school_classes#create', as: 'create_school_class'
+  
+  get '/school_classes/:id/edit', to: 'school_classes#edit', as: 'edit_school_class'
+
+  patch '/school_classes/:id', to: 'school_classes#update', as: 'update_school_class'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+student1 = Student.create(first_name: 'Matthew', last_name: 'Clark')
+student2 = Student.create(first_name: 'Charles', last_name: 'Darwin')
+
+geography = SchoolClass.create(title: 'Geography', room_number: 404)
+maths = SchoolClass.create(title: 'Mathematics', room_number: 200)
+
